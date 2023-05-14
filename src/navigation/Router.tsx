8ef11c18038dfa3699f8185ca303fbf2dbad64ac
Jorge Mora <jorgemg354@gmail.com>
2023-05-14 16:05:07 -0700

    Imported from project optimizacion-imagenes

--- src/navigation/Router.tsx
+++
@@ -1,7 +0,0 @@
-import RouterConfig from './RouterConfig';
-
-function Router() {
-	return <RouterConfig />;
-}
-
-export default Router;
--- src/views/HomeView/styles.ts
+++
@@ -1,5 +0,0 @@
-import styled from '@emotion/styled';
-
-export const VideoContainer = styled.div`
-	background: linear-gradient(#e66465, #9198e5);
-`;
