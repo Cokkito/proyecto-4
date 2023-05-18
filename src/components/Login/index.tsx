b04d251c5f3bd7268ade8bc0bce29af750a942b3
Jorge Mora <jorgemg354@gmail.com>
2023-05-17 17:01:19 -0700

    Change colors

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -41,7 +41,7 @@ function Login({ onClose, onSumbit }: Props) {
 							backgroundColor: (t) =>
 								t.palette.mode === 'light'
 									? t.palette.grey[50]
-									: t.palette.grey[900],
+									: t.palette.grey[800],
 							backgroundSize: 'cover',
 							backgroundPosition: 'center',
 						}}
